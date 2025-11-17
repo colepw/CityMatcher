5e5a468a529312c8d52365c609e120e6dc279f98
colepw <colepw7@gmail.com>
2025-11-17 03:48:09 -0800

    User class done

--- User.cpp
+++ User.cpp
@@ -1,4 +1,59 @@
 #include "User.h"
 #include <iostream>
 #include <string>
-#include <vector>
+#include <unordered_set>
+
+User::User() {}
+
+User::User(const std::unordered_set<std::string>& desiredCities, const std::string& firstName, const std::string& lastName, const std::string& email, const std::string& homeCity, const int& id)
+    : desiredCities_(desiredCities), firstName_(firstName), lastName_(lastName), email_(email), homeCity_(homeCity), id_(id) {}
+
+std::unordered_set<std::string> User::getDesiredCities() const {
+    return this->desiredCities_;
+}
+
+std::string User::getFirstName() const {
+    return this->firstName_;
+}
+
+std::string User::getLastName() const {
+    return this->lastName_;
+}
+
+std::string User::getName() const {
+    std::string name = this->firstName_ + " " + this->lastName_;
+    return name;
+}
+
+std::string User::getEmail() const {
+    return this->email_;
+}
+
+std::string User::getHomeCity() const {
+    return this->homeCity_;
+}
+
+int User::getId() const {
+    return this->id_;
+}
+
+void User::addCity(const std::string& city) {
+    this->desiredCities_.insert(city);
+}
+
+void User::setName(const std::string& first, const std::string& last) {
+    this->firstName_ = first;
+    this->lastName_ = last;
+}
+
+void User::setEmail(const std::string& email) {
+    this->email_ = email;
+}
+
+void User::setHomeCity(const std::string& city) {
+    this->homeCity_ = city;
+}
+
+void User::setId(const int& id) {
+    this->id_ = id;
+}
--- User.h
+++ User.h
@@ -3,15 +3,34 @@
 
 #include <iostream>
 #include <string>
-#include <vector>
+#include <unordered_set>
 
 class User {
     public:
+        User();
+        User(const std::unordered_set<std::string>& desiredCities, const std::string& firstName, const std::string& lastName, const std::string& email, const std::string& homeCity, const int& id);
+
+        std::unordered_set<std::string> getDesiredCities() const;
+        std::string getFirstName() const;
+        std::string getLastName() const;
+        std::string getName() const;
+        std::string getEmail() const;
+        std::string getHomeCity() const;
+        int getId() const;
+
+        void addCity(const std::string& city);
+        void setName(const std::string& first, const std::string& last);
+        void setEmail(const std::string& email);
+        void setHomeCity(const std::string& city);
+        void setId(const int& id);
 
     protected:
-        
+        std::unordered_set<std::string> desiredCities_;
+        std::string firstName_;
+        std::string lastName_;
+        std::string email_;
+        std::string homeCity_;
+        int id_;
 };
-
-
 
 #endif // USER_H
